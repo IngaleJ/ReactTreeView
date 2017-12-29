a6e1f8349c2162cbc4cd2318c29d5fcfac878805
IngaleJ <jayendraingale07@outlook.com>
2017-12-29 08:34:08 +0530

    changes in tree view

---
+++ src/component/MyTreeView.js
@@ -0,0 +1,24 @@
+import React, { Component } from 'react'
+import './MyTreeView.css'
+import RenderNodes from './RenderNode'
+
+class Tree extends Component {
+  render () {
+    let propsData = this.props.data
+    let childIcon = this.props.childIcon
+    let parentIcon = this.props.parentIcon
+    if (propsData.length > 1) {
+      return (
+        propsData.map((data) => <RenderNodes data={data} childIcon={childIcon} parentIcon={parentIcon} />)
+      )
+    } else {
+      return (
+        <RenderNodes data={propsData} childIcon={childIcon} parentIcon={parentIcon} />
+      )
+    }
+  }
+}
+
+export default Tree
+// document.getElementsByClassName("collapse").collapse('show');
+// ref :- http://jsfiddle.net/ssorallen/XX8mw/
